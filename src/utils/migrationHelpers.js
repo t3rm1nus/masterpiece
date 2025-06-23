f77fa81c000cdf11fb404e56c1fcd0c5fe64039c
t3rm1nus <elcharne@gmail.com>
2025-06-23 21:22:08 +0200

    estable

--- src/utils/migrationHelpers.js
+++ src/utils/migrationHelpers.js
@@ -8,15 +8,6 @@
  * @returns {object} - El nuevo estado migrado
  */
 export function migrateAppStoreState(state, version) {
-  // Ejemplo: migrar de v0 a v1 (añadir un campo nuevo)
-  // if (version === 0) {
-  //   return { ...state, nuevoCampo: valorPorDefecto };
-  // }
-  // Ejemplo: migrar de v1 a v2 (renombrar un campo)
-  // if (version === 1) {
-  //   const { oldField, ...rest } = state;
-  //   return { ...rest, newField: oldField };
-  // }
   return state;
 }
 
