c02d7ae70481dd12cfbbf5a4f77d529184500326
t3rm1nus <elcharne@gmail.com>
2025-07-15 14:22:49 +0200

    homePageHelpers

---
+++ src/utils/homePageHelpers.ts
@@ -0,0 +1,28 @@
+// Handlers and helpers for HomePage
+
+export const handleCategoryClick = (
+  setCategory: (category: string) => void,
+  setActiveSubcategory: (subcategory: string | null) => void,
+  setActiveLanguage: (lang: string) => void
+) => (category: string) => {
+  setCategory(category);
+  setActiveSubcategory(null);
+  setActiveLanguage('all');
+};
+
+export const handleSpanishCinemaToggle = (toggleSpanishCinema: () => void) => () => {
+  toggleSpanishCinema();
+};
+
+export const handleMasterpieceToggle = (toggleMasterpiece: () => void) => () => {
+  toggleMasterpiece();
+};
+
+export const handleItemClick = <T>(goToDetail: (item: T) => void) => (item: T) => {
+  goToDetail(item);
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+export const handleCloseDetail = (goBackFromDetail: () => void) => () => {
+  goBackFromDetail();
+}; 
